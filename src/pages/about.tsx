da8294cf3370ee49f2f8fb36832663f4bb31ee0f
fo-ji <57491651+fo-ji@users.noreply.github.com>
2021-01-30 12:10:21 +0900

    get query test

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import type { FC } from 'react'
-import { Link } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Button from '@material-ui/core/Button'
+import type { AboutPageQuery } from '../../types/graphql-types'
 
-const Page: FC = () => {
+interface PageProps {
+  data: AboutPageQuery
+}
+
+const Page: FC<PageProps> = ({ data }) => {
   return (
     <Layout>
-      <h1>About</h1>
+      <h1>About {data.site?.siteMetadata?.title ?? '(無題)'}</h1>
       <p>プロフィールページ</p>
       <Link to="/">
         <Button>Home</Button>
@@ -17,3 +22,13 @@ const Page: FC = () => {
 }
 
 export default Page
+
+export const query = graphql`
+  query AboutPage {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`

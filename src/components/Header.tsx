c8dee942eb104bbd676f0b83223d59f480511c4c
fo-ji <57491651+fo-ji@users.noreply.github.com>
2021-01-30 21:50:49 +0900

    implement header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,32 @@ import type { FC } from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 import type { HeaderComponentQuery } from '../../types/graphql-types'
 
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    background: theme.palette.secondary.dark,
+  },
+  title: {
+    color: theme.palette.secondary.light,
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  link: {
+    textDecoration: 'none',
+  },
+  button: {
+    color: theme.palette.primary.main,
+  },
+}))
+
 const Header: FC = () => {
   const data = useStaticQuery<HeaderComponentQuery>(graphql`
     query HeaderComponent {
@@ -14,12 +40,27 @@ const Header: FC = () => {
     }
   `)
 
+  const classes = useStyles()
+
   return (
-    <header>
-      <h1>
-        <Link to="/">{data.site?.siteMetadata?.title ?? '(無題)'}</Link>
-      </h1>
-    </header>
+    <AppBar position="static" className={classes.header}>
+      <Toolbar className={classes.content}>
+        <Typography variant="h4" className={classes.title}>
+          Portfolio..
+        </Typography>
+        <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+          <Link to="/about" className={classes.link}>
+            <Button className={classes.button}>About</Button>
+          </Link>
+          <Link to="/output" className={classes.link}>
+            <Button className={classes.button}>Output</Button>
+          </Link>
+          <Link to="/contact" className={classes.link}>
+            <Button className={classes.button}>Contact</Button>
+          </Link>
+        </ButtonGroup>
+      </Toolbar>
+    </AppBar>
   )
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import type { FC } from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { MuiThemeProvider } from '@material-ui/core'
+import { theme } from '../assets/theme'
+import '../assets/reset.css'
 
 const Layout: FC = ({ children }) => {
   return (
-    <div>
-      <Header />
-      <section>{children}</section>
-      <Footer />
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <Header />
+        <section>{children}</section>
+        <Footer />
+      </div>
+    </MuiThemeProvider>
   )
 }
 

90ecdf27cda7709356d6844c7ad6357ffa1ec3f2
fo-ji <57491651+fo-ji@users.noreply.github.com>
2021-01-30 17:35:30 +0900

    add material-ui muiteheme

---
+++ src/assets/theme.ts
@@ -0,0 +1,18 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#add9ff',
+      main: '#6cb6f5',
+      dark: '#5aa2e0',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#c5cee6',
+      main: '#a0a8bd',
+      dark: '#3a3d42',
+      contrastText: '#000',
+    },
+  },
+})
